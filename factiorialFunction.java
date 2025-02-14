4c1f00b7d4a77fc21c559ac2cc93eb30b9f3156d
Rabina Karki <techierabina@gmail.com>
2025-02-13 22:43:50 -0600

    factorial function program

---
+++ factiorialFunction.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class factiorialFunction {
+    
+    public static int fact(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            return n*fact(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to find the factorial: ");
+        int number=sc.nextInt();
+        int result=fact(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
