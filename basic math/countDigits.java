b841c3e02ae54cb1a64d2096c0156887424dbe49
Rabina Karki <techierabina@gmail.com>
2025-03-21 18:30:33 -0500

    digit count program

---
+++ basic math/countDigits.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class countDigits {
+
+    public static void digitCount(int number) {
+        if (number == 0) {
+            System.out.println("The number of digit is 1.");
+            return;
+        }
+        number=Math.abs(number);
+        int count = 0;
+        while (number != 0) {
+            count++;
+            number = number / 10;
+        }
+        System.out.println("The number of digits is: " + count);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of digits: ");
+        int number = sc.nextInt();
+        digitCount(number);
+
+    }
+}
